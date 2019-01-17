6a3d14ad14e1d64e8974caa62724d331edc33750
Cristiano Fagundes <fagundescristianof@gmail.com>
2019-01-17 17:05:24 -0200

    arange

--- carrinho.py
+++ carrinho.py
@@ -286,7 +286,7 @@ def inicializaSerial(caminho):
 
 
 def inicializaComunicacaoSerial():
-    for i in arange(0,10):
+    for i in range(0,10):
         inicializaSerial('/dev/ttyACM'+i)
     #inicializaSerial('/dev/ttyACM1')
 
